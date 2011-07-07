d4a57dd313dd2932b48550d97cf77b0ec2284f5f
Mariusz Nowak <medyk@medikoo.com>
2011-07-07 17:03:15 +0200

    Cleanup

--- test/chain/first.js
+++ test/chain/first.js
@@ -9,15 +9,15 @@ module.exports = {
 	"Return first to succeed": function (t, a, d) {
 		var x = {}
 		  , p1 = promise(x)
-		  , p2 = p1.then(k(2))
-		  , p3 = p2.then(k(3));
+		  , p2 = p1.then(k({}))
+		  , p3 = p2.then(k({}));
 		t(p1, p3, p3).then(function (r) {
 			a.equal(r, x); d();
 		}, neverCalled(a)).end();
 	},
 	"Error when all fail": function (t, a, d) {
 		var e = new Error('Test');
-		t(promise(e), promise(e), promise(e)).then(neverCalled(a, "DDD"), function (r) {
+		t(promise(e), promise(e), promise(e)).then(neverCalled(a), function (r) {
 			a.equal(r, e); d();
 		}).end();
 	},
