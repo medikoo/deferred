cab91766fcb3c0a5294d7f402d244c82e36d28ec
Mariusz Nowak <medyk@medikoo.com>
2011-09-22 11:37:36 +0200

    invoke extension should resolve in nextTick

--- test/ext/invoke.js
+++ test/ext/invoke.js
@@ -5,11 +5,14 @@ var deferred = require('../../lib/deferred');
 module.exports = {
 	"Deferred": function (a, d) {
 		var defer = deferred(), z = {}, x = { bar: z }
-		  , y = { foo: function (n) { return x[n]; } };
+		  , y = { foo: function (n) { return x[n]; } }
+		  , invoked = false;
 		defer.resolve(y).invoke('foo', 'bar')
 		(function (r) {
+			invoked = true;
 			a(r, z); d();
 		}, d).end();
+		a(invoked, false, "Resolve in next tick");
 	},
 	"Promise": function (a, d) {
 		var z = {}, x = { bar: z }
