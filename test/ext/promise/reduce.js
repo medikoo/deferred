3c792e23504409da3bf1954f40fe0192a258c742
Mariusz Nowak <mariusz@medikoo.com>
2012-01-05 17:00:13 +0100

    Added 'reduce' promise extensions (it's based on array's 'reduce' extension)

---
+++ test/ext/promise/reduce.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = function (t, a, d) {
+	t([t(2), t(3), 4]).reduce(function (arg1, arg2) {
+		return t(arg1*arg2);
+	}, t(1))(function (r) {
+		a(r, 24); d();
+	}, a.never);
+};
---
+++ test/ext/promise/utils/array.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var promise = require('../../../../lib/promise');
+
+module.exports = function (t, a, d) {
+	t('map', require('../../../../lib/ext/array/map'));
+
+	promise([promise(1), promise(2), 3]).map(function (res) {
+		return promise(res*res);
+	})(function (r) {
+		a.deep(r, [1, 4, 9]); d();
+	}, a.never);
+};
