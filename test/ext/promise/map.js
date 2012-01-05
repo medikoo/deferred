839f618770444eb553bbeeb7c93d4eef0d000dea
Mariusz Nowak <mariusz@medikoo.com>
2012-01-05 16:45:01 +0100

    Promise 'map' extension now uses 'map' extension for arrays

--- test/ext/promise/map.js
+++ test/ext/promise/map.js
@@ -1,46 +1,9 @@
 'use strict';
 
-var deferred = require('../../../lib/deferred');
-
-module.exports = {
-	"Deferred": {
-		"": function (a, d) {
-			var defer = deferred(), x = 0;
-			defer.resolve([1,2,3]).map(function (y) {
-				return x += y;
-			})
-			(function (r) {
-				a.deep(r, [1, 3, 6]); d();
-			}, d).end();
-		},
-		"Error": function (a, d) {
-			var defer = deferred(), x = 0, e = new Error();
-			defer.resolve([1,2,3]).map(function (y) {
-				return y == 1 ? e : y;
-			})
-			(a.never, function (r) {
-				a(r, e); d();
-			}).end();
-		}
-	},
-	"Promise": {
-		"": function (a, d) {
-			var x = 0;
-			deferred([1,2,3]).map(function (y) {
-				return x += y;
-			})
-			(function (r) {
-				a.deep(r, [1, 3, 6]); d();
-			}, d).end();
-		},
-		"Error": function (a, d) {
-			var x = 0, e = new Error();
-			deferred([1,2,3]).map(function (y) {
-				return y == 1 ? e : y;
-			})
-			(a.never, function (r) {
-				a(r, e); d();
-			}).end();
-		}
-	}
+module.exports = function (t, a, d) {
+	t([t(1), t(2), 3]).map(function (res) {
+		return t(res*res);
+	})(function (r) {
+		a.deep(r, [1, 4, 9]); d();
+	}, a.never);
 };
