2eafdd0a8df21314367a4f7b42b4102f21931c6e
Mariusz Nowak <mariusz@medikoo.com>
2012-01-05 20:45:49 +0100

    Removed all join methods. They're replaced by deferred.map & deferred.reduce

--- test/join/default.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var deferred = require('../../lib/deferred');
-
-module.exports = function (t, a, d) {
-	t(deferred(1), deferred(2), deferred(3))(function (r) {
-		a(r.join('|'), '1|2|3'); d();
-	}, a.never).end();
-};
