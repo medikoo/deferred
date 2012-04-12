97adc94a73996e30426f4b14865300ac5d3a4986
Mariusz Nowak <mariusz@medikoo.com>
2012-04-12 16:26:54 +0200

    We can now take nested properties through `get` extension. Cleaned tests, so they don't use async `done` handler.

--- test/ext/promise/get.js
+++ test/ext/promise/get.js
@@ -6,16 +6,22 @@ module.exports = {
 	"Deferred": function (a) {
 		var defer = deferred(), x = {}, y = { foo: x }
 		  , invoked = false;
-		defer.resolve(y).get('foo')(function (r) {
+		defer.resolve(y).get('foo').end(function (r) {
 			invoked = true;
 			a(r, x);
-		}).end();
+		}, null);
 		a(invoked, true, "Resolved in current tick");
 	},
-	"Promise": function (a, d) {
+	"Promise": function (a) {
 		var x = {}, y = { foo: x };
-		deferred(y).get('foo')(function (r) {
+		deferred(y).get('foo').end(function (r) {
 			a(r, x);
-		}).end(d);
+		}, null);
+	},
+	"Nested": function (a) {
+		var x = {}, y = { foo: { bar: x } };
+		deferred(y).get('foo', 'bar').end(function (r) {
+			a(r, x);
+		}, null);
 	}
 };
