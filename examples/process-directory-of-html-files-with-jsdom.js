a46ee1056e7160ac9a3da36cb3522b48f6f37e6e
Marc-Aurèle DARCHE <ma.darche@cynode.org>
2013-07-18 17:35:30 +0200

    Fixed according to previous comments
    * No use of *sync functions
    * More generic file name
    * Code convention change: dots are at begin of line, not at it's end

--- examples/process-directory-of-html-files-with-jsdom.js
+++ examples/process-directory-of-html-files-with-jsdom.js
@@ -4,10 +4,11 @@ var path = require('path');
 var fs = require('fs');
 var jsdom = require('jsdom');
 
+var deferred = require('deferred');
+
 // Convert Node.js async functions, into ones that return a promise
 var promisify = require('deferred').promisify;
 var readdir = promisify(fs.readdir);
-var readFile = promisify(fs.readFile, 1);
 
 // Put some HTML files in this folder
 var DOC_FOLDER_PATH = '/tmp/test';
@@ -16,43 +17,66 @@ var DOC_FOLDER_PATH = '/tmp/test';
  * Lists the content of the DOC_FOLDER_PATH, only at first level, ie not recursively.
  */
 function listDocs() {
-    readdir(DOC_FOLDER_PATH).
-        map(function(file_name) {
+    readdir(DOC_FOLDER_PATH)
+        .map(function(file_name) {
             // Unix "hidden" files (such as .DS_Store, etc.) are unwanted
-            if (file_name.indexOf('.') == 0) {
+            if (file_name.indexOf('.') === 0) {
                 return null;
             }
             var file_path = path.join(DOC_FOLDER_PATH, file_name);
-            var stats = fs.statSync(file_path);
-            // Directories are unwanted
-            if (!stats.isFile()) {
-                return null;
-            }
             var doc_metadata = {
-                path: file_path,
-                modified: stats.mtime
+                path: file_path
             };
             return doc_metadata;
-        }).
-        invoke('filter', function(doc_metadata) {
-            // Filtering to keep only the wanted files (files, not directories
-            // and not hidden).
+        })
+        .map(addModifiedForFiles)
+        .invoke('filter', function(doc_metadata) {
+            // Filtering to keep only the wanted files (not hidden, not
+            // directories).
             return !!doc_metadata;
-        }).
-        map(pGetMetadata).
-        done(function(result) {
+        })
+        .map(addContent)
+        .map(addTitleAndDescription)
+        .done(function(result) {
             logger.info("result:", result);
         }, function(err) {
             logger.error(err);
         });
-};
+}
 
-/**
- * @param {Object} doc_metadata contains the file path
- * @param {Function} callback
- */
-function getMetadata(doc_metadata, callback) {
-    var html = fs.readFileSync(doc_metadata.path);
+function addModifiedForFiles(doc_metadata) {
+    var def = deferred();
+
+    if (!doc_metadata) {
+        def.resolve(null);
+    }
+
+    fs.stat(doc_metadata.path, function(err, stats) {
+        // Directories are unwanted
+        if (!stats.isFile()) {
+            def.resolve(null);
+            return;
+        }
+        doc_metadata.modified = stats.mtime;
+        def.resolve(doc_metadata);
+    });
+
+    return def.promise;
+}
+
+function addContent(doc_metadata) {
+    var def = deferred();
+
+    fs.readFile(doc_metadata.path, function(err, data) {
+        doc_metadata.html = data;
+        def.resolve(doc_metadata);
+    });
+
+    return def.promise;
+}
+
+function addTitleAndDescription(doc_metadata) {
+    var def = deferred();
 
     // In the metadata we are interested in the file relative path and not the
     // file absolute path (this is just a choice for this example not related
@@ -61,12 +85,16 @@ function getMetadata(doc_metadata, callback) {
     doc_metadata.path = rpath;
 
     jsdom.env({
-        html: html,
+        html: doc_metadata.html,
         done: function (errors, window) {
             if (errors) {
                 logger.error(errors);
-                callback(errors);
+                def.resolve(new Error(errors));
             }
+
+            // Deleting the HTML content that is not needed anymore
+            delete doc_metadata.html;
+
             var elems;
             // Default the title to the file relative path
             var title = rpath;
@@ -83,10 +111,11 @@ function getMetadata(doc_metadata, callback) {
                 description = elems[0].textContent;
             }
             doc_metadata.description = description;
-            callback(doc_metadata);
+            def.resolve(doc_metadata);
         }
     });
+
+    return def.promise;
 }
-var pGetMetadata = promisify(getMetadata, 1);
 
 listDocs();
