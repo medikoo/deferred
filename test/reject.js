91be3e4dd88cc89c0e756e5e2fa266eb91b00204
Mariusz Nowak <mariusz@medikoo.com>
2014-04-03 13:08:15 +0200

    Tests for reject

---
+++ test/reject.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var isPromise = require('../lib/is-promise');
+
+module.exports = function (t, a) {
+	var rejected = t('elo');
+	a(isPromise(rejected), true, "Promise");
+	a(rejected.failed, true, "Rejected");
+	a(rejected.value, 'elo', "value");
+};
