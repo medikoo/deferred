c96f93c0e747b0b95239d9f2c0ce3dadde0e8da7
Mariusz Nowak <medyk@medikoo.com>
2015-01-07 16:54:32 +0100

    Force assimilation of pre v0.7 deferred promises

--- is-promise.js
+++ is-promise.js
@@ -3,5 +3,5 @@
 'use strict';
 
 module.exports = function (o) {
-	return (typeof o === 'function') && (typeof o.then === 'function');
+	return (typeof o === 'function') && (typeof o.then === 'function') && (o.end !== o.done);
 };
