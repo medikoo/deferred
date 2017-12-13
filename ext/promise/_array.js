0bab077f36c3e07b0b776b4980885b7cd89cc94e
Mariusz Nowak <medyk@medikoo.com>
2017-12-13 10:02:05 +0100

    style: prettier

--- ext/promise/_array.js
+++ ext/promise/_array.js
@@ -3,38 +3,43 @@
 "use strict";
 
 var callable = require("es5-ext/object/valid-callable")
-  , deferred = require("../../deferred")
+  , deferred = require("../../deferred");
 
-  , reject = deferred.reject;
+var reject = deferred.reject;
 
 module.exports = function (name, ext) {
-	deferred.extend(name, function (cb) {
-		var def;
-		(cb == null) || callable(cb);
-		if (!this.pending) this.pending = [];
-		def = deferred();
-		this.pending.push(name, [arguments, def.resolve, def.reject]);
-		return def.promise;
-	}, function (args, resolve, reject) {
-		var result;
-		if (this.failed) {
-			reject(this.value);
-			return;
+	deferred.extend(
+		name,
+		function (cb) {
+			var def;
+			cb == null || callable(cb);
+			if (!this.pending) this.pending = [];
+			def = deferred();
+			this.pending.push(name, [arguments, def.resolve, def.reject]);
+			return def.promise;
+		},
+		function (args, resolve, reject) {
+			var result;
+			if (this.failed) {
+				reject(this.value);
+				return;
+			}
+			try {
+				result = ext.apply(this.value, args);
+			} catch (e) {
+				reject(e);
+				return;
+			}
+			resolve(result);
+		},
+		function (cb) {
+			cb == null || callable(cb);
+			if (this.failed) return this;
+			try {
+				return ext.apply(this.value, arguments);
+			} catch (e) {
+				return reject(e);
+			}
 		}
-		try {
-			result = ext.apply(this.value, args);
-		} catch (e) {
-			reject(e);
-			return;
-		}
-		resolve(result);
-	}, function (cb) {
-		(cb == null) || callable(cb);
-		if (this.failed) return this;
-		try {
-			return ext.apply(this.value, arguments);
-		} catch (e) {
-			return reject(e);
-		}
-	});
+	);
 };
