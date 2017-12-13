03367ebb24c6fcbb749d6be7ba3ce1382794ead1
Mariusz Nowak <medyk@medikoo.com>
2017-12-13 11:03:59 +0100

    style: prettier

--- test/ext/array/find.js
+++ test/ext/array/find.js
@@ -6,28 +6,57 @@ var deferred  = require("../../../deferred")
 module.exports = function (t, a) {
 	var x = {}, y, z = 0, w;
 	a(t.call([]).valueOf(), undefined, "Empty, no cb");
-	a(t.call([], function () {
- return true;
-}).valueOf(), undefined, "Empty, cb");
+	a(
+		t
+			.call([], function () {
+				return true;
+			})
+			.valueOf(),
+		undefined,
+		"Empty, cb"
+	);
 	a(t.call([x]).valueOf(), x, "One, truthy, no cb");
 	a(t.call([0]).valueOf(), undefined, "One, falsy, no cb");
 	a(t.call([0, x]).valueOf(), x, "Two, falsy & truthy, no cb");
 	a(t.call([0, false]).valueOf(), undefined, "Two, falsy & falsy, no cb");
-	a(t.call(y = [false], function (a1, a2, a3) {
-		++z;
-		a(a1, false, "Argument");
-		a(a2, 0, "Index");
-		a(a3, y, "List");
-		a(this, x, "Context");
-		return true;
-	}, x).valueOf(), false, "One, falsy, cb truthy");
+	a(
+		t
+			.call(
+				y = [false],
+				function (a1, a2, a3) {
+					++z;
+					a(a1, false, "Argument");
+					a(a2, 0, "Index");
+					a(a3, y, "List");
+					a(this, x, "Context");
+					return true;
+				},
+				x
+			)
+			.valueOf(),
+		false,
+		"One, falsy, cb truthy"
+	);
 	a(z, 1, "Callback called");
-	a(t.call([1], function () {
- return false;
-}).valueOf(), undefined, "One, truthy, cb falsy");
-	a(t.call([1, 0], function (x) {
- ++z; return !x;
-}).valueOf(), 0, "Two, cb, Second truthy");
+	a(
+		t
+			.call([1], function () {
+				return false;
+			})
+			.valueOf(),
+		undefined,
+		"One, truthy, cb falsy"
+	);
+	a(
+		t
+			.call([1, 0], function (x) {
+				++z;
+				return !x;
+			})
+			.valueOf(),
+		0,
+		"Two, cb, Second truthy"
+	);
 	a(z, 3, "Callback called twice");
 
 	y = deferred();
