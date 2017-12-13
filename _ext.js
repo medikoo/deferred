aff24552a750a92e2a22970549f09d45f3c8d5da
Mariusz Nowak <medyk@medikoo.com>
2017-12-13 09:04:28 +0100

    style: prettier

--- _ext.js
+++ _ext.js
@@ -4,14 +4,13 @@ var callable   = require("es5-ext/object/valid-callable")
   , d          = require("d")
   , isCallable = require("es5-ext/object/is-callable")
   , ee         = require("event-emitter")
-  , isPromise  = require("./is-promise")
+  , isPromise  = require("./is-promise");
 
-  , create = Object.create, defineProperty = Object.defineProperty
-  , deferred, resolve, reject;
+var create = Object.create, defineProperty = Object.defineProperty, deferred, resolve, reject;
 
 module.exports = exports = function (name, unres, onres, res) {
 	name = String(name);
-	callable(res) && ((onres == null) || callable(onres)) && callable(unres);
+	callable(res) && (onres == null || callable(onres)) && callable(unres);
 	defineProperty(exports._unresolved, name, d(unres));
 	exports._onresolve[name] = onres;
 	defineProperty(exports._resolved, name, d(res));
@@ -20,26 +19,28 @@ module.exports = exports = function (name, unres, onres, res) {
 
 exports._names = ["done", "then", "valueOf"];
 
-exports._unresolved = ee(create(Function.prototype, {
-	then: d(function (win, fail) {
-		var def;
-		if (!this.pending) this.pending = [];
-		def = deferred();
-		this.pending.push("then", [win, fail, def.resolve, def.reject]);
-		return def.promise;
-	}),
-	done: d(function (win, fail) {
-		(win == null) || callable(win);
-		(fail == null) || callable(fail);
-		if (!this.pending) this.pending = [];
-		this.pending.push("done", arguments);
-	}),
-	resolved: d(false),
-	returnsPromise: d(true),
-	valueOf: d(function () {
- return this;
-})
-}));
+exports._unresolved = ee(
+	create(Function.prototype, {
+		then: d(function (win, fail) {
+			var def;
+			if (!this.pending) this.pending = [];
+			def = deferred();
+			this.pending.push("then", [win, fail, def.resolve, def.reject]);
+			return def.promise;
+		}),
+		done: d(function (win, fail) {
+			win == null || callable(win);
+			fail == null || callable(fail);
+			if (!this.pending) this.pending = [];
+			this.pending.push("done", arguments);
+		}),
+		resolved: d(false),
+		returnsPromise: d(true),
+		valueOf: d(function () {
+			return this;
+		})
+	})
+);
 
 exports._onresolve = {
 	then: function (win, fail, resolve, reject) {
@@ -60,8 +61,8 @@ exports._onresolve = {
 				return;
 			}
 			try {
- value = cb(this.value);
-} catch (e) {
+				value = cb(this.value);
+			} catch (e) {
 				reject(e);
 				return;
 			}
@@ -82,39 +83,41 @@ exports._onresolve = {
 	}
 };
 
-exports._resolved = ee(create(Function.prototype, {
-	then: d(function (win, fail) {
-		var value, cb = this.failed ? fail : win;
-		if (cb == null) return this;
-		if (isCallable(cb)) {
-			if (isPromise(cb)) return cb;
-			try {
- value = cb(this.value);
-} catch (e) {
- return reject(e);
-}
-			return resolve(value);
-		}
-		return resolve(cb);
-	}),
-	done: d(function (win, fail) {
-		(win == null) || callable(win);
-		(fail == null) || callable(fail);
-		if (this.failed) {
-			if (fail) {
-				fail(this.value);
-				return;
+exports._resolved = ee(
+	create(Function.prototype, {
+		then: d(function (win, fail) {
+			var value, cb = this.failed ? fail : win;
+			if (cb == null) return this;
+			if (isCallable(cb)) {
+				if (isPromise(cb)) return cb;
+				try {
+					value = cb(this.value);
+				} catch (e) {
+					return reject(e);
+				}
+				return resolve(value);
 			}
-			throw this.value;
-		}
-		if (win) win(this.value);
-	}),
-	resolved: d(true),
-	returnsPromise: d(true),
-	valueOf: d(function () {
- return this.value;
-})
-}));
+			return resolve(cb);
+		}),
+		done: d(function (win, fail) {
+			win == null || callable(win);
+			fail == null || callable(fail);
+			if (this.failed) {
+				if (fail) {
+					fail(this.value);
+					return;
+				}
+				throw this.value;
+			}
+			if (win) win(this.value);
+		}),
+		resolved: d(true),
+		returnsPromise: d(true),
+		valueOf: d(function () {
+			return this.value;
+		})
+	})
+);
 
 deferred = require("./deferred");
 resolve = deferred.resolve;
