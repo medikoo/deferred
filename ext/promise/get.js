2f18b58ff825c90b1ffaa46cb3314da31f444812
Mariusz Nowak <medyk@medikoo.com>
2017-12-13 10:05:42 +0100

    style: prettier

--- ext/promise/get.js
+++ ext/promise/get.js
@@ -7,39 +7,50 @@
 "use strict";
 
 var value    = require("es5-ext/object/valid-value")
-  , deferred = require("../../deferred")
+  , deferred = require("../../deferred");
 
-  , reduce = Array.prototype.reduce
-  , resolve = deferred.resolve, reject = deferred.reject;
+var reduce = Array.prototype.reduce, resolve = deferred.resolve, reject = deferred.reject;
 
-deferred.extend("get", function (/* …name*/) {
-	var def;
-	if (!this.pending) this.pending = [];
-	def = deferred();
-	this.pending.push("get", [arguments, def.resolve, def.reject]);
-	return def.promise;
-
-}, function (args, resolve, reject) {
-	var result;
-	if (this.failed) reject(this.value);
-	try {
-		result = reduce.call(args, function (obj, key) {
-			return value(obj)[String(key)];
-		}, this.value);
-	} catch (e) {
-		reject(e);
-		return;
+deferred.extend(
+	"get",
+	function (/* …name*/) {
+		var def;
+		if (!this.pending) this.pending = [];
+		def = deferred();
+		this.pending.push("get", [arguments, def.resolve, def.reject]);
+		return def.promise;
+	},
+	function (args, resolve, reject) {
+		var result;
+		if (this.failed) reject(this.value);
+		try {
+			result = reduce.call(
+				args,
+				function (obj, key) {
+					return value(obj)[String(key)];
+				},
+				this.value
+			);
+		} catch (e) {
+			reject(e);
+			return;
+		}
+		resolve(result);
+	},
+	function (/* …name*/) {
+		var result;
+		if (this.failed) return this;
+		try {
+			result = reduce.call(
+				arguments,
+				function (obj, key) {
+					return value(obj)[String(key)];
+				},
+				this.value
+			);
+		} catch (e) {
+			return reject(e);
+		}
+		return resolve(result);
 	}
-	resolve(result);
-}, function (/* …name*/) {
-	var result;
-	if (this.failed) return this;
-	try {
-		result = reduce.call(arguments, function (obj, key) {
-			return value(obj)[String(key)];
-		}, this.value);
-	} catch (e) {
-		return reject(e);
-	}
-	return resolve(result);
-});
+);
