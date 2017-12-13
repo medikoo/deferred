0c2c1d86c46e8b2dfaef9ac32e8e7482b366ab8b
Mariusz Nowak <medyk@medikoo.com>
2017-12-13 09:44:44 +0100

    refactor: lint

--- ext/_process-arguments.js
+++ ext/_process-arguments.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var arrayOf    = require("es5-ext/array/of")
+  , isValue    = require("es5-ext/object/is-value")
   , deferred   = require("../deferred")
   , isPromise  = require("../is-promise")
   , assimilate = require("../assimilate");
@@ -8,18 +9,20 @@ var arrayOf    = require("es5-ext/array/of")
 var push = Array.prototype.push, slice = Array.prototype.slice;
 
 module.exports = function (args, length) {
-	var i, l, arg;
-	if (length != null && args.length !== length) {
+	var i, arg;
+	if (isValue(length) && args.length !== length) {
 		args = slice.call(args, 0, length);
 		if (args.length < length) {
 			push.apply(args, new Array(length - args.length));
 		}
+	} else {
+		length = args.length;
 	}
-	for (i = 0, l = args.length; i < l; ++i) {
+	for (i = 0; i < length; ++i) {
 		arg = assimilate(args[i]);
 		if (isPromise(arg)) {
 			if (!arg.resolved) {
-				if (l > 1) return deferred.apply(null, args);
+				if (length > 1) return deferred.apply(null, args);
 				return arg(arrayOf);
 			}
 			if (arg.failed) return arg;
