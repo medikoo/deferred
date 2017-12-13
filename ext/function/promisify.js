6fc4eff4bd1a4b0b1d617628a06946d185fcfee1
Mariusz Nowak <medyk@medikoo.com>
2017-12-13 10:01:50 +0100

    refactor: lint

--- ext/function/promisify.js
+++ ext/function/promisify.js
@@ -2,14 +2,16 @@
 
 "use strict";
 
-var callable  = require("es5-ext/object/valid-callable")
-  , callAsync = require("./call-async")._base;
+var isValue         = require("es5-ext/object/is-value")
+  , callable        = require("es5-ext/object/valid-callable")
+  , toNaturalNumber = require("es5-ext/number/to-pos-integer")
+  , callAsync       = require("./call-async")._base;
 
 module.exports = function (length) {
 	var fn, result;
 	fn = callable(this);
 	if (fn.returnsPromise) return fn;
-	if (length != null) length >>>= 0;
+	if (isValue(length)) length = toNaturalNumber(length);
 	result = function () {
 		return callAsync(fn, length, this, arguments);
 	};
