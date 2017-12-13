742a31464bd56c6522ff85c8278d735b9c60e1d0
Mariusz Nowak <medyk@medikoo.com>
2017-12-13 10:04:21 +0100

    refactor: lint

--- ext/promise/catch.js
+++ ext/promise/catch.js
@@ -10,54 +10,59 @@ var isCallable = require("es5-ext/object/is-callable")
   , validValue = require("es5-ext/object/valid-value")
   , deferred   = require("../../deferred")
   , isPromise  = require("../../is-promise")
+  , resolve    = deferred.resolve
+  , reject     = deferred.reject;
 
-  , resolve = deferred.resolve, reject = deferred.reject;
-
-deferred.extend("catch", function (cb) {
-	var def;
-	validValue(cb);
-	if (!this.pending) this.pending = [];
-	def = deferred();
-	this.pending.push("catch", [cb, def.resolve, def.reject]);
-	return def.promise;
-}, function (cb, resolve, reject) {
-	var value;
-	if (!this.failed) {
-		resolve(this.value);
-		return;
-	}
-	if (isCallable(cb)) {
-		if (isPromise(cb)) {
-			if (cb.resolved) {
-				if (cb.failed) reject(cb.value);
-				else resolve(cb.value);
-			} else {
-				cb.done(resolve, reject);
+deferred.extend(
+	"catch",
+	function (cb) {
+		var def;
+		validValue(cb);
+		if (!this.pending) this.pending = [];
+		def = deferred();
+		this.pending.push("catch", [cb, def.resolve, def.reject]);
+		return def.promise;
+	},
+	function (cb, localResolve, localReject) {
+		var value;
+		if (!this.failed) {
+			localResolve(this.value);
+			return;
+		}
+		if (isCallable(cb)) {
+			if (isPromise(cb)) {
+				if (cb.resolved) {
+					if (cb.failed) localReject(cb.value);
+					else localResolve(cb.value);
+				} else {
+					cb.done(localResolve, localReject);
+				}
+				return;
 			}
+			try {
+				value = cb(this.value);
+			} catch (e) {
+				localReject(e);
+				return;
+			}
+			localResolve(value);
 			return;
 		}
-		try {
- value = cb(this.value);
-} catch (e) {
-			reject(e);
-			return;
+		localResolve(cb);
+	},
+	function (cb) {
+		var value;
+		validValue(cb);
+		if (!this.failed) return this;
+		if (isCallable(cb)) {
+			if (isPromise(cb)) return cb;
+			try {
+				value = cb(this.value);
+			} catch (e) {
+				return reject(e);
+			}
+			return resolve(value);
 		}
-		resolve(value);
-		return;
+		return resolve(cb);
 	}
-	resolve(cb);
-}, function (cb) {
-	var value;
-	validValue(cb);
-	if (!this.failed) return this;
-	if (isCallable(cb)) {
-		if (isPromise(cb)) return cb;
-		try {
- value = cb(this.value);
-} catch (e) {
-			return reject(e);
-		}
-		return resolve(value);
-	}
-	return resolve(cb);
-});
+);
