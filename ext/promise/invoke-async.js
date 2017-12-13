7628ffcca225ef334c9985ad448603ad74f02fe1
Mariusz Nowak <medyk@medikoo.com>
2017-12-13 10:49:16 +0100

    refactor: lint

--- ext/promise/invoke-async.js
+++ ext/promise/invoke-async.js
@@ -1,3 +1,5 @@
+/* eslint max-statements: "off" */
+
 // 'invokeAsync' - Promise extension
 //
 // promise.invokeAsync(name[, arg0[, arg1[, ...]]])
@@ -10,6 +12,7 @@
 "use strict";
 
 var toArray          = require("es5-ext/array/to-array")
+  , isValue          = require("es5-ext/object/is-value")
   , isCallable       = require("es5-ext/object/is-callable")
   , deferred         = require("../../deferred")
   , isPromise        = require("../../is-promise")
@@ -20,49 +23,50 @@ var slice = Array.prototype.slice
   , reject = deferred.reject
   , applyFn;
 
-applyFn = function (fn, args, resolve, reject) {
+applyFn = function (fn, args, localResolve, localReject) {
 	var result;
 	if (fn.returnsPromise) {
 		try {
 			result = apply.call(fn, this, args);
 		} catch (e) {
-			reject(e);
+			localReject(e);
 			return;
 		}
-		return resolve(result);
+		localResolve(result);
+		return;
 	}
-	args = toArray(args).concat(function (error, result) {
-		if (error == null) {
-			resolve(arguments.length > 2 ? slice.call(arguments, 1) : result);
+	args = toArray(args).concat(function (error, localResult) {
+		if (isValue(error)) {
+			localReject(error);
 		} else {
-			reject(error);
+			localResolve(arguments.length > 2 ? slice.call(arguments, 1) : localResult);
 		}
 	});
 	try {
 		apply.call(fn, this, args);
 	} catch (e2) {
-		reject(e2);
+		localReject(e2);
 	}
 };
 
 deferred.extend(
 	"invokeAsync",
-	function (method/*, …args*/) {
+	function (methodIgnored/*, …args*/) {
 		var def;
 		if (!this.pending) this.pending = [];
 		def = deferred();
 		this.pending.push("invokeAsync", [arguments, def.resolve, def.reject]);
 		return def.promise;
 	},
-	function (args, resolve, reject) {
+	function (args, localResolve, localReject) {
 		var fn;
 		if (this.failed) {
-			reject(this.value);
+			localReject(this.value);
 			return;
 		}
 
-		if (this.value == null) {
-			reject(new TypeError("Cannot use null or undefined"));
+		if (!isValue(this.value)) {
+			localReject(new TypeError("Cannot use null or undefined"));
 			return;
 		}
 
@@ -70,7 +74,7 @@ deferred.extend(
 		if (!isCallable(fn)) {
 			fn = String(fn);
 			if (!isCallable(this.value[fn])) {
-				reject(new TypeError(fn + " is not a function"));
+				localReject(new TypeError(fn + " is not a function"));
 				return;
 			}
 			fn = this.value[fn];
@@ -79,24 +83,24 @@ deferred.extend(
 		args = processArguments(slice.call(args, 1));
 		if (isPromise(args)) {
 			if (args.failed) {
-				reject(args.value);
+				localReject(args.value);
 				return;
 			}
 			args.done(
-				function (args) {
-					applyFn.call(this, fn, args, resolve, reject);
+				function (argsResolved) {
+					applyFn.call(this, fn, argsResolved, localResolve, localReject);
 				}.bind(this.value),
-				reject
+				localReject
 			);
 		} else {
-			applyFn.call(this.value, fn, args, resolve, reject);
+			applyFn.call(this.value, fn, args, localResolve, localReject);
 		}
 	},
 	function (method/*, …args*/) {
 		var args, def;
 		if (this.failed) return this;
 
-		if (this.value == null) {
+		if (!isValue(this.value)) {
 			return reject(new TypeError("Cannot use null or undefined"));
 		}
 
@@ -113,16 +117,16 @@ deferred.extend(
 			if (args.failed) return args;
 			def = deferred();
 			args.done(
-				function (args) {
-					applyFn.call(this, method, args, def.resolve, def.reject);
+				function (argsResolved) {
+					applyFn.call(this, method, argsResolved, def.resolve, def.reject);
 				}.bind(this.value),
 				def.reject
 			);
-		} else if (!method.returnsPromise) {
+		} else if (method.returnsPromise) {
+			return applyFn.call(this.value, method, args, deferred, reject);
+		} else {
 			def = deferred();
 			applyFn.call(this.value, method, args, def.resolve, def.reject);
-		} else {
-			return applyFn.call(this.value, method, args, deferred, reject);
 		}
 		return def.promise;
 	}
