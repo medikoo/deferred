4c80f6d79670f1c094f71659c2fd5bad54780069
Mariusz Nowak <medyk@medikoo.com>
2017-12-13 11:42:27 +0100

    style: prettier

--- index.js
+++ index.js
@@ -7,23 +7,27 @@
 var call   = Function.prototype.call
   , assign = require("es5-ext/object/assign");
 
-module.exports = assign(require("./deferred"), {
-	invokeAsync: require("./invoke-async"),
-	isPromise: require("./is-promise"),
-	dynamicQueue: require("./dynamic-queue"),
-	validPromise: require("./valid-promise"),
-	callAsync: call.bind(require("./ext/function/call-async")),
-	delay: call.bind(require("./ext/function/delay")),
-	gate: call.bind(require("./ext/function/gate")),
-	monitor: require("./monitor"),
-	promisify: call.bind(require("./ext/function/promisify")),
-	promisifySync: call.bind(require("./ext/function/promisify-sync")),
-	every: call.bind(require("./ext/array/every")),
-	find: call.bind(require("./ext/array/find")),
-	map: call.bind(require("./ext/array/map")),
-	reduce: call.bind(require("./ext/array/reduce")),
-	some: call.bind(require("./ext/array/some"))
-}, require("./profiler"));
+module.exports = assign(
+	require("./deferred"),
+	{
+		invokeAsync: require("./invoke-async"),
+		isPromise: require("./is-promise"),
+		dynamicQueue: require("./dynamic-queue"),
+		validPromise: require("./valid-promise"),
+		callAsync: call.bind(require("./ext/function/call-async")),
+		delay: call.bind(require("./ext/function/delay")),
+		gate: call.bind(require("./ext/function/gate")),
+		monitor: require("./monitor"),
+		promisify: call.bind(require("./ext/function/promisify")),
+		promisifySync: call.bind(require("./ext/function/promisify-sync")),
+		every: call.bind(require("./ext/array/every")),
+		find: call.bind(require("./ext/array/find")),
+		map: call.bind(require("./ext/array/map")),
+		reduce: call.bind(require("./ext/array/reduce")),
+		some: call.bind(require("./ext/array/some"))
+	},
+	require("./profiler")
+);
 
 require("./ext/promise/aside");
 require("./ext/promise/catch");
