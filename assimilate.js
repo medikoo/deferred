e29cf1cf39735dab9c48c3e69d84868cc05d1018
Mariusz Nowak <medyk@medikoo.com>
2017-12-13 09:11:21 +0100

    style: prettier

--- assimilate.js
+++ assimilate.js
@@ -2,11 +2,10 @@
 
 "use strict";
 
-var isObject  = require("es5-ext/object/is-object")
-  , isPromise = require("./is-promise")
-  , deferred  = require("./deferred")
-  , nextTick  = require("next-tick")
-
+var isObject       = require("es5-ext/object/is-object")
+  , isPromise      = require("./is-promise")
+  , deferred       = require("./deferred")
+  , nextTick       = require("next-tick")
   , getPrototypeOf = Object.getPrototypeOf;
 
 module.exports = function self(value) {
@@ -26,11 +25,11 @@ module.exports = function self(value) {
 	} catch (ignore) {}
 	def = deferred();
 	resolve = function (value) {
- def.resolve(self(value));
-};
+		def.resolve(self(value));
+	};
 	reject = function (value) {
- def.reject(value);
-};
+		def.reject(value);
+	};
 	if (typeof done === "function") {
 		try {
 			done.call(value, resolve, reject);
@@ -40,15 +39,19 @@ module.exports = function self(value) {
 		return def.promise;
 	}
 	try {
-		then.call(value, function (value) {
- nextTick(function () {
-			resolve(value);
-		});
-}, function (value) {
- nextTick(function () {
-			reject(value);
-		});
-});
+		then.call(
+			value,
+			function (value) {
+				nextTick(function () {
+					resolve(value);
+				});
+			},
+			function (value) {
+				nextTick(function () {
+					reject(value);
+				});
+			}
+		);
 	} catch (e) {
 		return def.reject(e);
 	}
