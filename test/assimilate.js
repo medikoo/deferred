193cd56c6ba320a8bc5680345dd7e0d98f261c59
Mariusz Nowak <medyk@medikoo.com>
2018-11-06 11:28:45 +0100

    style: prettier

--- test/assimilate.js
+++ test/assimilate.js
@@ -1,23 +1,19 @@
 "use strict";
 
-var deferred = require("../deferred")
+var deferred = require("../deferred");
 
-  , x = {};
+var x = {};
 
 module.exports = {
 	"then": {
 		Resolve: function (t, a, d) {
-			t({ then: function (resolve) {
-				resolve(x);
-			} }).done(function (value) {
+			t({ then: function (resolve) { resolve(x); } }).done(function (value) {
 				a(value, x);
 				d();
 			}, a.never);
 		},
 		Reject: function (t, a, d) {
-			t({ then: function (resolve, reject) {
-				reject(x);
-			} }).done(a.never, function (value) {
+			t({ then: function (resolve, reject) { reject(x); } }).done(a.never, function (value) {
 				a(value, x);
 				d();
 			});
@@ -25,30 +21,25 @@ module.exports = {
 	},
 	"done": {
 		Resolve: function (t, a) {
-			t({ then: function () {},
-done: function (resolve) {
-				resolve(x);
-			} }).done(function (value) {
-				a(value, x);
-			}, a.never);
+			t({ then: function () {}, done: function (resolve) { resolve(x); } }).done(function (
+				value
+			) { a(value, x); }, a.never);
 		},
 		Reject: function (t, a) {
-			t({ then: function () {},
-done: function (resolve, reject) {
-				reject(x);
-			} }).done(a.never, function (value) {
-				a(value, x);
-			});
+			t({ then: function () {}, done: function (resolve, reject) { reject(x); } }).done(
+				a.never,
+				function (value) { a(value, x); }
+			);
 		}
 	},
 	"via then": function (t, a, d) {
-		deferred("marko").then(function () {
-			return { then: function (resolve) {
-				resolve("foreign");
-			} };
-		}).done(function (value) {
-			a(value, "foreign");
-			d();
-		});
+		deferred("marko")
+			.then(function () {
+				return { then: function (resolve) { resolve("foreign"); } };
+			})
+			.done(function (value) {
+				a(value, "foreign");
+				d();
+			});
 	}
 };
