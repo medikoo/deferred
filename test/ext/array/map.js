0f28849ab1769c8bbc47aa6df8f050aca9ec0f16
Mariusz Nowak <medyk@medikoo.com>
2018-12-27 17:12:37 +0100

    style: prettier

--- test/ext/array/map.js
+++ test/ext/array/map.js
@@ -1,8 +1,7 @@
 "use strict";
 
 var deferred = require("../../../deferred")
-
-  , reject = deferred.reject;
+  , reject   = deferred.reject;
 
 module.exports = function (t) {
 	var x = {}, y = {}, z = {}, e = new Error("Error");
@@ -10,49 +9,43 @@ module.exports = function (t) {
 	return {
 		"Empty": {
 			"": function (a, d) {
-				t.call([])(function (result) {
-					a.deep(result, []);
-				}, a.never).done(d, d);
+				t.call([])(function (result) { a.deep(result, []); }, a.never).done(d, d);
 			},
 			"Callback": function (a, d) {
-				t.call([], a.never)(function (result) {
-					a.deep(result, []);
-				}, a.never).done(d, d);
+				t.call([], a.never)(function (result) { a.deep(result, []); }, a.never).done(d, d);
 			}
 		},
 		"One": {
 			Value: {
 				"": function (a, d) {
-					t.call([x])(function (result) {
-						a.deep(result, [x]);
-					}, a.never).done(d, d);
+					t.call([x])(function (result) { a.deep(result, [x]); }, a.never).done(d, d);
 				},
 				"Callback": function (a, d) {
 					var list = [x];
-					t.call(list, function (arg, index, target) {
-						a(arg, x, "Argument");
-						a(index, 0, "Index");
-						a(target, list, "Target");
-						a(this, x, "Context");
-						return y;
-					}, x)(function (result) {
-						a.deep(result, [y]);
-					}, a.never).done(d, d);
+					t.call(
+						list,
+						function (arg, index, target) {
+							a(arg, x, "Argument");
+							a(index, 0, "Index");
+							a(target, list, "Target");
+							a(this, x, "Context");
+							return y;
+						},
+						x
+					)(function (result) { a.deep(result, [y]); }, a.never).done(d, d);
 				}
 			},
 			Promise: {
 				"": function (a, d) {
-					t.call([deferred(x)])(function (result) {
-						a.deep(result, [x]);
-					}, a.never).done(d, d);
+					t.call([deferred(x)])(function (result) { a.deep(result, [x]); }, a.never).done(
+						d, d
+					);
 				},
 				"Callback": function (a, d) {
 					t.call([deferred(x)], function (arg) {
 						a(arg, x, "Argument");
 						return y;
-					})(function (result) {
-						a.deep(result, [y]);
-					}, a.never).done(d, d);
+					})(function (result) { a.deep(result, [y]); }, a.never).done(d, d);
 				}
 			}
 		},
@@ -70,9 +63,9 @@ module.exports = function (t) {
 					}, a.never).done(d, d);
 				},
 				"Error promise": function (a, d) {
-					t.call([x, y, deferred(e), z, deferred(y)])(a.never, function (res) {
-						a(res, e);
-					}, a.never).done(d, d);
+					t.call([x, y, deferred(e), z, deferred(y)])(
+						a.never, function (res) { a(res, e); }, a.never
+					).done(d, d);
 				}
 			},
 			"Callback": {
@@ -83,30 +76,22 @@ module.exports = function (t) {
 							a.never();
 						}
 						throw e;
-					})(a.never, function (res) {
-						a(res, e);
-					}).done(d, d);
+					})(a.never, function (res) { a(res, e); }).done(d, d);
 				},
 				"Error via input": function (a, d) {
-					t.call([x, y, deferred(e), z], function () {
-						return x;
-					})(a.never, function (res) {
-						a(res, e);
-					}).done(d, d);
+					t.call([x, y, deferred(e), z], function () { return x; })(a.never, function (
+						res
+					) { a(res, e); }).done(d, d);
 				},
 				"Values & Promises": function (a, d) {
 					t.call([1, deferred(2), 3, deferred(4), 5], function (val) {
 						return val * val;
-					})(function (res) {
-						a.deep(res, [1, 4, 9, 16, 25]);
-					}, a.never).done(d, d);
+					})(function (res) { a.deep(res, [1, 4, 9, 16, 25]); }, a.never).done(d, d);
 				},
 				"Values & Promises, through promise": function (a, d) {
 					t.call([1, deferred(2), 3, deferred(4), 5], function (val) {
 						return deferred(val * val);
-					})(function (res) {
-						a.deep(res, [1, 4, 9, 16, 25]);
-					}, a.never).done(d, d);
+					})(function (res) { a.deep(res, [1, 4, 9, 16, 25]); }, a.never).done(d, d);
 				}
 			}
 		},
@@ -117,16 +102,12 @@ module.exports = function (t) {
 			// case anymore
 
 			var d = deferred();
-			t.call([d.promise]).done(function () {
-				throw new Error("ERROR");
-			});
+			t.call([d.promise]).done(function () { throw new Error("ERROR"); });
 			a.throws(d.resolve);
 		},
 		"Return rejected promise in callback": function (a) {
 			var e = new Error("Some error");
-			t.call([1, 2], function () {
-				return reject(e);
-			}).done(a.never, function (err) {
+			t.call([1, 2], function () { return reject(e); }).done(a.never, function (err) {
 				a(err, e);
 			});
 		}
